e58d8509a17dd86908cb30d6de2151a5123625e2
bapostol91 <bapostol91@yahoo.com>
2023-09-27 10:15:39 +0300

    REfactor docs

--- openapi/code_samples/java/api_company_{cui}.java
+++
@@ -1,8 +0,0 @@
-OkHttpClient client=new OkHttpClient();
-
-Request request=new Request.Builder()
-.url("https://app.metricbiz.com/v1/api/companies/"+cui+"?currency="+currency)
-.get()
-.build();
-
-Response response=client.newCall(request).execute();
